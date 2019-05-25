f82884422cbc42c2f69a63c51a34b01569a1a054
mohitsharma98 <mohitsharma30898@gmail.com>
2019-05-25 16:10:46 +0530

    Second Commit

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var expressHbs = require('express-handlebars');
 var app = express();
+
+mongoose.connect('localhost:27017/shopping');
 
 // view engine setup
 app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
